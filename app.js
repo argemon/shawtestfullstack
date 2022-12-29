b589f38c24e4c771ea04168043aad44b241a8ded
Argemon <edson_argemon@hotmail.com>
2022-12-29 17:32:56 -0400

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const { randomUUID } = require("crypto");
+const { request } = require("http");
+const { response } = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const users = [];
+
+app.post("/users", (request, response) => {
+    //users
+    
+    const { username, details, repos } = request.body;
+
+    const users = {
+        username,
+        details,
+        repos,
+        id: randomUUID(),
+    };
+
+    users.push(users);
+
+    return response.json(users);
+});
+
 app.get("/users", (request, response) => {
-    return response.json({
-        message: "users",
-    });
+    return response.json(users);
+});
+
+app.get("/users/:id", (request, response) => {
+    const { id } = request.params;
+    const users = users.find((users) => users.id);
+    return response.json(users);
 });
 
 app.listen(4002, () => console.log("server on 4002"));
